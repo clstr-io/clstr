8705be8dd12f980b4962972739ff73470435c164
Stephen Mwangi <mail@stephenmwangi.com>
2026-04-03 21:41:04 +0300

    fix: add language option to init

--- images/embed.go
+++ images/embed.go
@@ -3,4 +3,15 @@ package images
 import "embed"
 
 //go:embed */Dockerfile
-var Images embed.FS
+var FS embed.FS
+
+// Dockerfile returns the Dockerfile for the given language, falling back to
+// the generic template if no built-in exists for that language.
+func Dockerfile(lang string) ([]byte, error) {
+	data, err := FS.ReadFile(lang + "/Dockerfile")
+	if err == nil {
+		return data, nil
+	}
+
+	return FS.ReadFile("template/Dockerfile")
+}
